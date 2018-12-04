0469362c39218f53d962e508689eb9ddd4d0c401
Amrit Pandey <amritpandey@protonmail.com>
2018-12-04 21:36:41 +0530

    render template, passing data into templates while rendering

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World"
+posts = [
+    {
+        "author" : "Amrit Pandey",
+        "title" : "First ever blog post",
+        "content" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ipsam ipsum repellendus distinctio illo ipsa voluptatum voluptatibus eaque dolorum incidunt quasi obcaecati dolores, corrupti, amet, necessitatibus blanditiis? Nisi, a aliquid.",
+        "date_posted" : "April 20, 2018"
+    },
+    {
+        "author" : "Janet Doe",
+        "title" : "Second ever blog post",
+        "content" : "Lorem ipsum dolor sit amet coFirstnsectetur adipisicing elit. Libero ipsam ipsum repellendus distinctio illo ipsa voluptatum voluptatibus eaque dolorum incidunt quasi obcaecati dolores, corrupti, amet, necessitatibus blanditiis? Nisi, a aliquid.",
+        "date_posted" : "April 21, 2018"
+    }
+]
 
-app.run(debug = True)
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template("home.html", posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
