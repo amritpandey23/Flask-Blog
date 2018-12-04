6c3a082e36ed842aac62557c30f56726be800a8f
Amrit Pandey <amritpandey@protonmail.com>
2018-12-05 00:15:06 +0530

    hello methods for home route is now named "home", added two routes for register and login

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "aaa269055db1c78ee6d59ef9"
 
 posts = [
     {
@@ -19,12 +21,22 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template("home.html", posts = posts, title="Home")
 
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="login", form=form)
 
 if __name__ == "__main__":
     app.run(debug = True)
