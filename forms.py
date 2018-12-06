bde28928f9b28c1ad36701cce6524c337982374c
Amrit Pandey <amritpandey@protonmail.com>
2018-12-06 19:26:49 +0530

    some comments

--- forms.py
+++ forms.py
@@ -1,7 +1,13 @@
+# Flask data models that will store data submitted by users
+# through forms can easily be created with flask_wtf package
+# and FlaskForm module. These also help in rendering the
+# form HTML.
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+# REGISTRATION FORM MODEL
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -9,6 +15,7 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
 
+# LOGIN FORM MODEL
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
