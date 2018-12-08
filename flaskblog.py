217f8ac6077fe7640941b1362640ef882a352c8d
Amrit Pandey <amritpandey@protonmail.com>
2018-12-08 18:19:08 +0530

    connected User and Post table models(sqlite: one in many relation)

--- flaskblog.py
+++ flaskblog.py
@@ -23,6 +23,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
     # __repr__ is a built-in function used to compute 
     # the "official" string reputation of an object.
     def __repr__():
@@ -33,7 +34,7 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=DataTime.utcnow)
     content = db.Column(db.Text, nullable=False)
-
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     def __repr__():
         return f"Post: ('{self.title', '{self.date_posted}')"
 
