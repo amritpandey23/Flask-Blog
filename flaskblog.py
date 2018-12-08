e00fb9dfacc0ca8cdb4e35d5123b8a3358b10a00
Amrit Pandey <amritpandey@protonmail.com>
2018-12-08 17:45:00 +0530

    comment update, nothing important

--- flaskblog.py
+++ flaskblog.py
@@ -72,7 +72,7 @@ def login():
 
 # Every module in python has a special attribute called __name__ .
 # The value of __name__  attribute is set to '__main__'  when 
-# module run as main program. Otherwise the value of __name__  
-# is set to contain the name of the module.
+# module run as current program. Otherwise the value of __name__  
+# is set to contain the name of the module which is currently running.
 if __name__ == "__main__":
     app.run(debug = True)
