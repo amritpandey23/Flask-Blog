9206c066bfe414ead5c774cefacd56b7c929eafe
Amrit Pandey <amritpandey@protonmail.com>
2018-12-09 21:10:09 +0530

    init file for package

---
+++ flaskblog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+# SQLAlchemy is an ORM(Object Relational Mapping) 
+# https://en.wikipedia.org/wiki/Object-relational_mapping
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Anything that requires encryption (for safe-keeping against 
+# tampering by attackers) requires the secret key to be set.
+app.config["SECRET_KEY"] = "aaa269055db1c78ee6d59ef9"
+
+# The database URI that should be used for the connection
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+# Create database
+db = SQLAlchemy(app)
+
+from flaskblog import routes
