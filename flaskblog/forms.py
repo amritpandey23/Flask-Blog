03ce85517a80cabbef6485c0c63e417d8c33d609
Amrit Pandey <amritpandey@protonmail.com>
2018-12-14 23:56:00 +0530

    validate fields for duplicate data in db and then raise validation error.

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -5,7 +5,8 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 # REGISTRATION FORM MODEL
 class RegistrationForm(FlaskForm):
@@ -14,6 +15,25 @@ class RegistrationForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
+
+    # We first need to check whether the details submitted by the
+    # end user is unique to the data we already have. For eg.
+    # `username` and `email` should be unique to each user, if
+    # not the sqlite will throw an error.
+
+    # Two functions have been defined to cross check if same
+    # `username` or `email` exist as entered by end-user.
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(f"'{user.username}' is already taken, please use another!")
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(f"'{user.email}' already exist, please use another!")
+
 
 # LOGIN FORM MODEL
 class LoginForm(FlaskForm):
