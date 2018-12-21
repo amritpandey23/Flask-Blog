563db54dde65f9cac96e8002312d6a1ea0f2b5de
a-pandey <spanit007@outlook.com>
2018-12-21 22:47:37 +0530

    info class to flash message from login_manager

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -18,5 +18,6 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
